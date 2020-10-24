ab077293e1acece9bf0336f8df1b7e6961692245
Martin Achtermann <mdacht777@gmail.com>
2020-10-24 12:50:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,17 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-function greet(){
-  return "Good Morning";
+function greet(input){
+  return input
+  // if (input){
+  // return "Good Morning";
+  // }
+  // elseif (input){
+  // return "Good Morning";
+  // }
+  // else (input){
+  // return "Good Morning";
+  // }
 }
 /* Write your implementation of displayMessage() */
 function displayMessage(){
